fdc959eca218bfb7a1879cb0f534cf95b942c17a
M Zeeshan Mehdi <zeeshanmehdi.dev@gmail.com>
2025-08-04 00:00:39 +0500

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -18,7 +18,6 @@ import mongodbLogo from './assets/tech_logo/mongodb.png';
 import firebaseLogo from './assets/tech_logo/firebase.png';
 import cLogo from './assets/tech_logo/c.png';
 import cppLogo from './assets/tech_logo/cpp.png';
-import javaLogo from './assets/tech_logo/java.png';
 import pythonLogo from './assets/tech_logo/python.png';
 import typescriptLogo from './assets/tech_logo/typescript.png';
 import gitLogo from './assets/tech_logo/git.png';
@@ -38,7 +37,6 @@ import kakushinLogo from './assets/company_logo/kakushin.png';
 // Education Section Logo's
 import vuLogo from './assets/education_logo/vu_logo.png';
 import bsaLogo from './assets/education_logo/bsa_logo.png';
-import vpsLogo from './assets/education_logo/vps_logo.png';
 
 // Project Section Logo's
 import githubdetLogo from './assets/work_logo/github_det.png';
@@ -174,23 +172,22 @@ export const SkillsInfo = [
   export const projects = [
     {
       id: 0,
-      title: "GitHub Profile Detective",
+      title: "StudyBlocks Learnmix",
       description:
-        "A powerful and user-friendly React.js application designed to uncover and showcase detailed GitHub profile information. Simply enter a GitHub username, and the app fetches comprehensive data, including profile stats, repositories, followers, and contributions. The intuitive interface ensures a seamless experience, making it a must-visit tool for developers and recruiters.",
+        "This platform is an education marketplace that utilizes a Meduajs backend and a Next.js frontend. Creators serve as vendors, designing educational products for sale. They earn commissions each time a customer makes a purchase. The platform aims to provide a seamless and user-friendly experience for both creators and customers.",
       image: githubdetLogo,
-      tags: ["HTML", "CSS", "JavaScript", "React JS", "API"],
-      github: "https://github.com/codingmastr/GitHub-Profile-Search-App-Using-React-JS",
-      webapp: "https://githubprofiledetective.netlify.app/",
+      tags: ["Medusa JS", "Next.js", "Next.js", "JavaScript", "React JS", "PostgreSQL"],
+      webapp: "https://studyblock.up.railway.app",
     },
     {
       id: 1,
-      title: "CS Prep",
+      title: "Sookup Marketplace",
       description:
-        "A full-stack quiz-based platform designed for GATE/UGC NET students to practice previous year questions and create customized tests. The platform offers comprehensive profile stats and detailed results, helping students track and improve their preparation journey effectively.",
+        "This marketplace is built with Mercurjs and Next.js. It is a marketplace for selling products, with creators serving as vendors and customers buying products. it has admin dashboard to manage vendors and customers. The platform aims to provide a seamless and user-friendly experience for sellers, customers, and administrators.",
       image: csprepLogo,
-      tags: ["React JS", "Node.js", "MongoDB", "Express", "HTML", "CSS", "JavaScript"],
+      tags: ["Medusa JS", "Next.js", "PostgreSQL", "Mercurjs", "TypeScript", "TailwindCSS"],
       github: "https://github.com/codingmastr/CSPrep",
-      webapp: "https://csprep.netlify.app/",
+      webapp: "https://sookup-frontend-production.up.railway.app",
     },
     {
       id: 2,
